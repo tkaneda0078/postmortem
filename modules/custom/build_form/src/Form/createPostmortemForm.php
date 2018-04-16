f60be2c1e171786006b5b355814c0b7275db54ec
t-kaneda <t-kaneda@t-kanedanoMacBook-puro.local>
2018-04-16 21:00:27 +0900

    first commit

---
+++ modules/custom/build_form/src/Form/createPostmortemForm.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\build_form\Form\createPostmortemForm.
+ */
+
+namespace Drupal\build_form\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+
+/**
+ * Class createPostmortemForm.
+ *
+ * @package Drupal\build_form\Form
+ */
+class createPostmortemForm extends FormBase
+{
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId()
+  {
+    return 'create_postmortem_form';
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
+
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+
+  }
+
+}
